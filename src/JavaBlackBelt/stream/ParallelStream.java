507f86b69640a8c03cc09c6b5c9cf48655bbc540
Artur Makaryuk <art.makaryuk@gmail.com>
2023-03-15 16:18:13 +0700

    workstream

---
+++ src/JavaBlackBelt/stream/ParallelStream.java
@@ -0,0 +1,34 @@
+package JavaBlackBelt.stream;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ParallelStream {
+
+    // 1 2 3 4 .... 1000000000
+    // 1 250.000000
+    // 250.000001 500.000000
+    // 500.000001 750.000000
+    // 750.000001 1000000000
+    public static void main(String[] args) {
+        List<Double> list = new ArrayList<>();
+        list.add(10.0);
+        list.add(5.0);
+        list.add(1.0);
+        list.add(0.25);
+
+//        double sumResult = list.parallelStream()
+//                .reduce((accumulator, element) -> accumulator+element)
+//                .get();
+//        System.out.println("sumResult = " + sumResult);
+
+        // некорректное выполнение
+        double divisionResult = list.parallelStream()
+                .reduce((accumulator, element) -> accumulator/element)
+                .get();
+        System.out.println("sumResult = " + divisionResult);
+        // (10/5) = 2
+        // (1/0.25) = 4
+        // 2/4 = 0.5
+    }
+}
