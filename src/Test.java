deae174e12dec31dac64d4a3687900169c957c81
Artur Makaryuk <art.makaryuk@gmail.com>
2023-04-03 18:37:29 +0700

    kata

--- src/Test.java
+++ src/Test.java
@@ -1,61 +1,52 @@
 import java.util.Arrays;
 
-public class Test {
+public class Test{
+
     public static void main(String[] args) {
-        String h = "Monday";
-        System.out.println(checkWeekend(h));
-        Box box1 = new Box(10, 10);
-        box1.changeWidth(20);
-        System.out.println(box1.toString());
-        int[] array = {1, 2, 3, 4, 5};
-        box1.changeArray(array);
-        System.out.println(Arrays.toString(array));
+
+        Account acc1 = new Account("1876", 4500);
+        Account acc2 = new Account("3476", 1500);
+
+        Transaction<Account> tran1 = new Transaction<Account>(acc1,acc2, 4000);
+        tran1.execute();
+        tran1 = new Transaction<Account>(acc1,acc2, 4000);
+        tran1.execute();
     }
-
-    public static String checkWeekend(String weekday) {
-        //Какая-то логика реализованная
-        return weekday;
-    }
-
-
 }
-
-class Box {
-    int width;
-    int height;
-
-    public Box(int width, int height) {
-        this.width = width;
-        this.height = height;
+class Transaction<T extends Account>{
+    private T from;     // с какого счета перевод
+    private T to;       // на какой счет перевод
+    private int sum;    // сумма перевода
+    Transaction(T from, T to, int sum){
+        this.from = from;
+        this.to = to;
+        this.sum = sum;
     }
-
-    public int changeWidth(int width) {
-        return this.width = width;
-    }
-
-    @Override
-    public String toString() {
-        return "Box{" +
-                "width=" + width +
-                ", height=" + height +
-                '}';
-    }
-
-    public int[] changeArray(int[] arr) {
-        for (int i = 0; i < arr.length; i++) {
-            arr[i] = arr[i] + 1;
+    public void execute(){
+        if (from.getSum() > sum)
+        {
+            from.setSum(from.getSum() - sum);
+            to.setSum(to.getSum() + sum);
+            System.out.printf("Account %s: %d \nAccount %s: %d \n",
+                    from.getId(), from.getSum(),to.getId(), to.getSum());
         }
-        System.out.println(Arrays.toString(arr));
-        return arr;
+        else{
+            System.out.printf("Operation is invalid");
+        }
     }
 }
-
-abstract class Cat {
-    public static void get() {
-
+class Account{
+    private String id;
+    private int sum;
+    Account(String id, int sum){
+        this.id = id;
+        this.sum = sum;
     }
-}
 
+    public String getId() { return id; }
+    public int getSum() { return sum; }
+    public void setSum(int sum) { this.sum = sum; }
+}
 
 
 
