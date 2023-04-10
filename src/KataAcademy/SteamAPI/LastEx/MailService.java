3c1a331fe573c24d2cb729a6c02d0261fb380daa
Artur Makaryuk <art.makaryuk@gmail.com>
2023-04-10 14:32:27 +0700

    workstream

--- src/KataAcademy/SteamAPI/LastEx/MailService.java
+++ src/KataAcademy/SteamAPI/LastEx/MailService.java
@@ -8,21 +8,23 @@ public class MailService<T> implements Consumer<Sendable<T>> {
     private Map<String, List<T>> mailBox = new HashMap<>() {
         @Override
         public List<T> get(Object key) {
-            if (this.containsKey(key)) {
-                return super.get(key);
-            } else {
-                return Collections.emptyList();
-            }
-
+//            if (this.containsKey(key)) {
+//                return super.get(key);
+//            } else {
+//                return Collections.emptyList();
+//            }
+            return this.containsKey(key) ? super.get(key) : Collections.emptyList();
         }
     };
 
     @Override
     public void accept(Sendable<T> sendable) {
-        List<T> list = mailBox.get(sendable.getTo());
-        if (list.size() == 0) {
-            list = new ArrayList<>();
-        }
+//        List<T> list = mailBox.get(sendable.getTo());
+//        if (list.size() == 0) {
+//            list = new ArrayList<>();
+//        }
+//        list.add(sendable.getContent());
+        List<T> list = mailBox.getOrDefault(sendable.getTo(), new ArrayList<>());
         list.add(sendable.getContent());
         mailBox.put(sendable.getTo(), list);
     }
